af4d229ca3861f18be5efcb13e02548bbf85c06f
Wetterkurve contributors <contributors@example.org>
2026-09-06 20:36:23 +0200

    Build Android with JDK 21 toolchain

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -64,7 +64,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
